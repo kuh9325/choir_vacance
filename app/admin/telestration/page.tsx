5e05005ab8d29d114ccb26f5766596acf361dece
Junho Kim <kuh9325@kau.kr>
2026-09-11 20:47:56 +0900

    Show telestration score controls in admin page

--- app/admin/telestration/page.tsx
+++ app/admin/telestration/page.tsx
@@ -1,2 +1,9 @@
 import { TelestrationApp } from '@/components/TelestrationApp';
-export default function TelestrationAdminPage() { return <TelestrationApp mode="admin" />; }
+import { TelestrationScoreDock } from '@/components/TelestrationScoreDock';
+
+export default function TelestrationAdminPage() {
+  return <>
+    <TelestrationApp mode="admin" />
+    <TelestrationScoreDock />
+  </>;
+}
