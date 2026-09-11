81fd78524a540b827b402843c7b489f16ad6cf85
Junho Kim <kuh9325@kau.kr>
2026-09-11 21:04:39 +0900

    Return charades admin to base screen after completion

--- app/admin/charades/page.tsx
+++ app/admin/charades/page.tsx
@@ -1,5 +1,9 @@
 import { CharadesApp } from '@/components/CharadesApp';
+import { GameAdminReturnBridge } from '@/components/GameAdminReturnBridge';
 
 export default function CharadesAdminPage() {
-  return <CharadesApp mode="admin" />;
+  return <>
+    <CharadesApp mode="admin" />
+    <GameAdminReturnBridge mode="charades" />
+  </>;
 }
