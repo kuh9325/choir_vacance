2dc00293519a34698099314ecc1d53c0362ad4fd
Junho Kim <kuh9325@kau.kr>
2026-09-11 22:12:41 +0900

    Expose team setup from admin home

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,11 @@
 import { AdminGameLauncher } from '@/components/AdminGameLauncher';
 import { ScoreApp } from '@/components/ScoreApp';
+import { TeamSetupDock } from '@/components/TeamSetupDock';
 
 export default function AdminPage() {
   return <>
     <ScoreApp mode="admin" />
     <AdminGameLauncher />
+    <TeamSetupDock />
   </>;
 }
