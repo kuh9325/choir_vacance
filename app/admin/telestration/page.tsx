86a463fb8fb577d61b2356b378f1c7a6ae08bdfe
Junho Kim <kuh9325@kau.kr>
2026-09-11 22:36:37 +0900

    Use dynamic telestration score dock

--- app/admin/telestration/page.tsx
+++ app/admin/telestration/page.tsx
@@ -1,11 +1,11 @@
 import { GameAdminReturnBridge } from '@/components/GameAdminReturnBridge';
 import { TelestrationDynamicApp } from '@/components/TelestrationDynamicApp';
-import { TelestrationScoreDock } from '@/components/TelestrationScoreDock';
+import { TelestrationScoreDockDynamic } from '@/components/TelestrationScoreDockDynamic';
 
 export default function TelestrationAdminPage() {
   return <>
     <TelestrationDynamicApp mode="admin" />
-    <TelestrationScoreDock />
+    <TelestrationScoreDockDynamic />
     <GameAdminReturnBridge mode="telestration" />
   </>;
 }
