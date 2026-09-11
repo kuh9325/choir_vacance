680903e5e1f52e89f8d378d0200816ee25ffed76
Junho Kim <kuh9325@kau.kr>
2026-09-12 01:33:56 +0900

    Show charades theme picker on waiting display

--- app/display/charades/page.tsx
+++ app/display/charades/page.tsx
@@ -1,5 +1,9 @@
 import { CharadesApp } from '@/components/CharadesApp';
+import { CharadesThemePicker } from '@/components/CharadesThemePicker';
 
 export default function CharadesDisplayPage() {
-  return <CharadesApp mode="display" />;
+  return <>
+    <CharadesApp mode="display" />
+    <CharadesThemePicker />
+  </>;
 }
